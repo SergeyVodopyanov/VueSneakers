cc2ff95595e9aed9be541950d9fbc5d13ce7441d
Sergey-LinuxPC-Git <vodopyanov03@bk.ru>
2024-09-11 21:46:32 +0300

    Разработка поиска + фильтрация товаров (вёрстка + функционал)

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,30 @@ import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
-  <Drawer></Drawer>
+  <!-- <DrawerHead></DrawerHead> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <Header></Header>
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <div class="flex gap-4">
+          <select class="h-11 px-3 border rounded-md outline-none">
+            <option>По названию</option>
+            <option>По возростанию цены</option>
+            <option>По убыванию цены</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="Search" />
+            <input
+              class="border rounded-md py-2 pl-12 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск"
+            />
+          </div>
+        </div>
+      </div>
+
       <CardList></CardList>
     </div>
   </div>
